f24fb6fc435561683262450a080142f2d8f4e24c
tuantuan <dangoakachan@foxmail.com>
2012-01-11 16:13:21 +0800

    add enable_scale option

--- image_autoshow.user.js
+++ image_autoshow.user.js
@@ -10,6 +10,9 @@
 // ==/UserScript==
 
 (function() {
+    /* Enable image scale when hover */
+    var enable_scale = true;
+
     /* Zoom in/out scale */
     var scale = 1.5;
 
@@ -58,26 +61,28 @@
         if (images.length == 0)
             return;
 
-        /* Add customized style for images */
-        var css = "\
-            a.clickloadImage img, img.resizeable {\
-                background-color: #FFFFFF;\
-                border: 1px solid #EFEFEF;\
-                display: block;\
-                padding: 4px;\
-                margin: 0 auto 10px auto;\
-            }\
-            a.clickloadImage img:hover, img.resizeable:hover {\
-                -webkit-transform: scale(%scale%);\
-                -moz-transform: scale(%scale%);\
-                -webkit-box-shadow: 0 3px 6px rgba(0,0,0,.25);\
-                -moz-box-shadow: 0 3px 6px rgba(0,0,0,.25);\
-                position: relative;\
-                z-index: 5;\
-            }\
-            ".replace(/%scale%/g, scale);
+        if (enable_scale) {
+            var css = "\
+                a.clickloadImage img, img.resizeable {\
+                    background-color: #FFFFFF;\
+                    border: 1px solid #EFEFEF;\
+                    display: block;\
+                    padding: 4px;\
+                    margin: 0 auto 10px auto;\
+                }\
+                a.clickloadImage img:hover, img.resizeable:hover {\
+                    -webkit-transform: scale(%scale%);\
+                    -moz-transform: scale(%scale%);\
+                    -webkit-box-shadow: 0 3px 6px rgba(0,0,0,.25);\
+                    -moz-box-shadow: 0 3px 6px rgba(0,0,0,.25);\
+                    position: relative;\
+                    z-index: 5;\
+                }".replace(/%scale%/g, scale);
 
-        add_style(css);
+            /* Add customized style for images */
+            add_style(css);
+        } else
+            scale = 1;
 
         /* Set max width to the td node's width */
         max_width = get_parent(images[0], "td").offsetWidth;
@@ -91,6 +96,7 @@
             /* Collapsed images */
             if (image.className != "resizeable") {
                 var parent = image.parentNode;
+                parent.setAttribute("onclick", "");
                 parent.innerHTML = "<img src='" + parent.href + "' border='0'>";
                 image = parent.firstElementChild;
             }
@@ -102,7 +108,6 @@
                 /* Set image width to min(cur_width, max_width) */
                 e.target.setAttribute("width", Math.min(cur_width, max_width));
             }, false);
-
         });
     }
 
